6585f67b5244e28b64a3fd2239131290715dbfbe
fedtoscano <federicotoscano611@gmail.com>
2024-07-01 16:08:21 +0200

    scaffolding

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- STYLE.CSS -->
+    <link rel="stylesheet" href="./style/style.css">
+    <title>PHP-JSON Todo List</title>
+</head>
+<body>
+    
+<div id="app">
+    <header>
+        <h1>TODO LIST</h1>
+    </header>
+
+    <main>
+
+    </main>
+
+
+    <!-- VUE 3 CDN -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- AXIOS CDN -->
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <!-- MAIN.JS PATH -->
+    <script src="./script/main.js"></script>
+</div>
+</body>
+</html>
